c04bdd3deeb0d68a6e1e5f70ef01c8e905b7b8cb
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-04-30 13:27:11 -0400

    Create sql-demo.sql

---
+++ azure-synapse-analytics/code/sql-demo.sql
@@ -0,0 +1,38 @@
+## serverless SQL pool SQL query
+
+SELECT
+    TOP 100 *
+FROM
+    OPENROWSET(
+            BULK 'https://datatechlake.dfs.core.windows.net/users/NYCTripSmall.parquet',
+        FORMAT='PARQUET'
+    ) AS [result]
+
+
+## Dedicated SQL pool
+
+CREATE TABLE [dbo].[Geography]
+(
+    [GeographyID] int NOT NULL,
+    [ZipCodeBKey] varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [County] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [City] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [State] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [Country] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [ZipCode] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+WITH
+(
+    DISTRIBUTION = ROUND_ROBIN,
+    CLUSTERED COLUMNSTORE INDEX
+);
+
+COPY INTO [dbo].[Geography]
+FROM 'https://nytaxiblob.blob.core.windows.net/2013/Geography'
+WITH
+(
+    FILE_TYPE = 'CSV',
+	FIELDTERMINATOR = ',',
+	FIELDQUOTE = ''
+)
+

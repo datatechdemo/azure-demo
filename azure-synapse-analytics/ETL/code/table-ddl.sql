f5e16bfcf0048249f178f03a21a7786111a897b7
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-08 11:31:01 -0400

    Update table-ddl.sql

--- azure-synapse-analytics/ETL/code/table-ddl.sql
+++ azure-synapse-analytics/ETL/code/table-ddl.sql
@@ -1,1 +1,16 @@
-
+CREATE TABLE dbo.greentaxi (
+VendorID varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL, 
+PassengerCount bigint,
+TripDistance bigint,
+PickupTime DATETIME, 
+DropTime DATETIME, 
+PickupLocationId varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL, 
+DropLocationId varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,   
+TotalAmount bigint, 
+PaymentType varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL, 
+TripYear integer,  
+TripMonth integer,  
+TripDay integer,  
+TripTimeInMinutes double,
+TripType varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL 
+);

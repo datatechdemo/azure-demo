d1de08be49ecff46f0e2506b2269a0291e3031fd
datatechdemo <82328348+datatechdemo@users.noreply.github.com>
2021-05-25 19:42:23 -0400

    Update file-source.py

--- azure-synapse-analytics-streaming/code/file-source.py
+++ azure-synapse-analytics-streaming/code/file-source.py
@@ -3,26 +3,38 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.master("local") \
-                               .appName("Console sink") \
+    spark = SparkSession.builder.appName('File sink') \
                                .getOrCreate()
+    
     spark.sparkContext.setLogLevel("ERROR")
     
-    schema = StructType([StructField("Date", StringType(), False), \
-    					 StructField("Article_ID", StringType(), False), \
-    					 StructField("Country_Code", StringType(), False), \
-    					 StructField("Sold_Units", IntegerType(), False)])
-    
+    schema = StructType([StructField('car', StringType(), True),\
+                         StructField('price', DoubleType(), True),\
+                         StructField('body', StringType(), True),\
+                         StructField('mileage', DoubleType(), True),\
+                         StructField('engV', StringType(), True),\
+                         StructField('engType', StringType(), True),\
+                         StructField('registration', StringType(), True),\
+                         StructField('year', IntegerType(), True),\
+                         StructField('model', StringType(), True),\
+                         StructField('drive', StringType(), True)])
+    # Replace the path with your location of ADLS2 in Azure synpase analytics  
     fileStreamDf = spark.readStream \
-					 		   .option("header", "true")\
-					 		   .schema(schema)\
-					 		   .csv("abfss://users@datatechstorage.dfs.core.windows.net/streaming_data")
+			 .option('header', 'true')\
+			 .schema(schema)\
+			 .csv("path")
 
-    aggDf = fileStreamDf.select('Article_ID','Country_Code','Sold_Units') 
+    selectedDf = fileStreamDf.select('*')
 
-    query = aggDf.writeStream.outputMode('append') \
-                .format('csv') \
-                .option('path','abfss://users@datatechstorage.dfs.core.windows.net/output_data') \
-                .option('checkpointLocation','abfss://users@datatechstorage.dfs.core.windows.net/checkpoint') \
+    cleanDf = selectedDf.select('car','price','body','mileage','engType','year','model','drive') \
+                         .withColumnRenamed('car','make') \
+                         .withColumnRenamed('mileage', 'distance')
+    
+    # Replace the path with your location of ADLS2 in Azure synpase analytics  
+    query = cleanDf.writeStream.outputMode('append') \
+                .format('json') \
+                .option('path','abfss://users@datatechstorage.dfs.core.windows.net/output_data/') \
+                .option('checkpointLocation','path') \
                 .start()
+   
     query.awaitTermination()
